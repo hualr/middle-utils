4fb019dc4c2c93e73bb2c576b3ef11eac9d8b9fa
hualr <hualefeng@outlook.com>
2022-07-19 01:07:49 +0800

    python kafka

--- flask_demo/demo1.py
+++ flask_demo/demo1.py
@@ -1,10 +1,31 @@
+import functools
+
 from flask import Flask, request, json, make_response, jsonify
 import logging
 
+from prometheus_client import Counter, Gauge
 from prometheus_flask_exporter import PrometheusMetrics
+from werkzeug.sansio import response
+
+
+def make_bold(func):
+    def wrapper(*args, **kwargs):
+        a=10
+        counter.labels(a=request.method,b=request.path).inc()
+        results= func(*args, **kwargs)
+        b=9
+        time_waste.labels(a=request.method,b=request.path).set(a-b)
+        return results
+
+    return wrapper
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+counter = Counter("test", "test document1", ['a', 'b'])
+time_waste = Gauge("time", "test document1", ['a', 'b'])
+metrics = PrometheusMetrics(app, defaults_prefix='', default_latency_as_histogram=False, metrics_decorator=make_bold)
+
+
+
 
 # logger = logging.getLogger('atp_log')
 # logger.setLevel(logging.DEBUG)
@@ -30,11 +51,12 @@ def hi2():
         "name": 1
     }
 
-    return make_response(data)\
-
+    return make_response(data) \
+ \
+ \
 @app.route('/hi3')
 def hi3():
-    print(1/0)
+    print(1 / 0)
     data = {
         "name": 1
     }
